4ef31f9173eafe836b336fa8a4518ceb977015fe
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-07-09 14:49:53 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ export const setupServer = ()=>{
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status:200,
       message :"Successfully found contacts!",
       data:contacts,
     });
@@ -35,11 +36,13 @@ export const setupServer = ()=>{
 
     if(!contact){
       res.status(404).json({
+        status:404,
         message:"Contact not found!"
       });
       return;
     }
     res.status(200).json({
+      status: 200,
       message :`Successfully found contact with id ${contactId}!`,
       data:contact,
     });
