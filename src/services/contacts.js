e4936eb7fed72d5ec883327e42a65d06a2288447
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-07-28 22:26:50 +0300

    new

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,26 @@
 import { ContactsCollection } from "../db/contacts.js";
+import {calculatePaginationData} from "../utils/calculatePaginationData.js";
+import {SORT_ORDER} from "../constants/index.js";
+export const getAllContacts = async ({ page = 1, perPage=10, sortOrder=SORT_ORDER.ASC, sortBy="_id",filter={} }) => {
+  const limit = perPage;
+  const skip = (page-1)*perPage;
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+  const contactsQuery = ContactsCollection.find();
+
+  if(filter.isFavourite){
+    contactsQuery.where("isFavourite").equals(filter.isFavourite);
+  }
+  if(filter.contactType){
+    contactsQuery.where("contactType").equals(filter.contactType);
+  }
+
+
+  const [contactsCount,contacts] = await Promise.all([ContactsCollection.find().merge(contactsQuery).countDocuments(),contactsQuery.skip(skip).limit(limit).sort({[sortBy]:sortOrder}).exec()]);
+  const paginationData = calculatePaginationData(contactsCount,page,perPage);
+  return {
+    data:contacts,
+    ...paginationData
+  };
 };
 export const getContactsById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
