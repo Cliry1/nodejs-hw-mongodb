d5a1a807c6b108e48ed419345de3ecfe23bd555d
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-07-28 22:26:28 +0300

    Update contacts.js
    new

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,16 +1,25 @@
 import { getAllContacts, getContactsById, createContact, updateContact, deleteContact } from '../services/contacts.js';
 import createHttpError from 'http-errors';
-
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import {parseSortParams} from "../utils/parseSortParams.js";
+import {parseFilterParams} from "../utils/parseFilterParams.js";
 export const getAllContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
-
+  const {page, perPage} = parsePaginationParams(req.query);
+  const {sortBy, sortOrder} = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortOrder,
+    sortBy,
+    filter
+  });
   res.status(200).json({
     status:200,
     message :"Successfully found contacts!",
     data:contacts,
   });
 };
-
 
 export const getContactsByIdController = async (req, res,next) => {
   const {contactId} = req.params;
@@ -42,7 +51,7 @@ export const createContactController = async (req, res) => {
 export const patchContactController = async (req, res, next) => {
   const {contactId} = req.params;
   const {contact} = await updateContact(contactId, req.body);
-
+  console.log(contact);
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
     return;
