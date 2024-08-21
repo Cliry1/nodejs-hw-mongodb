12f175283d2500454af635c66383f88d2e2b5c28
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-08-21 04:55:48 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -14,8 +14,8 @@ router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
-router.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetTokenController));
+router.post('/send-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetTokenController));
 
-router.post('/reset-password',validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
+router.post('/reset-pwd',validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 export default router;
