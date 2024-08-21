e101d6f8c72c5c0f3744460376d264114a209829
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-08-21 23:28:34 +0300

    new

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import {notFoundHandler} from "./middlewares/notFoundHandler.js";
 import cookieParser from 'cookie-parser';
 import { env } from './utils/env.js';
 import { UPLOAD_DIR } from './constants/index.js';
-
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 dotenv.config();
@@ -22,7 +22,7 @@ export const setupServer = ()=>{
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
-
+  app.use('/api-docs', swaggerDocs());
   
   app.use(
     pino({
